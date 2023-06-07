9778f5ce33e35abdfe07a2733b7e423cd7b7801a
LionelMessi2903 <adribanano2904@gmail.com>
2023-06-06 20:09:33 -0500

    tarea nestJS

---
+++ src/model/curso.model.ts
@@ -0,0 +1,39 @@
+import { Column,  DeleteDateColumn, Entity, PrimaryGeneratedColumn, CreateDateColumn } from "typeorm";
+import { UpdateDateColumn } from "typeorm/decorator/columns/UpdateDateColumn";
+
+@Entity('curso', { schema: 'curso' })
+export class CursoEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @CreateDateColumn({
+    name: 'create_at', //trabajamos con guion bajo
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP',
+  })
+  createAt: Date;
+  @UpdateDateColumn({
+    name: 'update_at',
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP',
+  })
+  updateAt: Date;
+  @DeleteDateColumn({
+    name: 'delete_at',
+    type: 'timestamp',
+    nullable: true,
+  })
+  deleteAt: Date;
+  @Column('varchar',{
+    name:'paralelo',
+    nullable:false,
+    comment:'nombre del paralelo'
+  })
+  paralelo:string;
+  @Column('numeric',{
+    name:'numeroParticipantes',
+    nullable:false,
+    comment:'numero de participantes'
+  })
+  numeroParticipantes:number;
+}
